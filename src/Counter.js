f8816bf1ee12bd6c9652c4405a751b5aa53039d6
Marc Löhe <marc@marcloehe.de>
2017-05-13 10:16:58 +0200

    Extract reducer from Counter component

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
+const reducer = (state = { value: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+};
+
 class Counter extends Component {
-  state = { value: 0 };
+  state = reducer(undefined, {});
+
+  dispatch(action) {
+    this.setState(prevState => reducer(prevState, action));
+  }
 
   increment = () => {
-    this.setState(state => ({
-      value: state.value + 1,
-    }));
+    this.dispatch({ type: 'INCREMENT' });
   };
 
   decrement = () => {
-    this.setState(state => ({
-      value: state.value - 1,
-    }));
+    this.dispatch({ type: 'DECREMENT' });
   };
 
   render() {
